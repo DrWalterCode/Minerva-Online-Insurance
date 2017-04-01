aaca4de8decc0760eb3367f22803ecad22040e17
walter <walter@health263.systems>
2017-04-01 10:56:57 +0300

    login page commit

--- Minerva Online Insurance/Minerva Online Insurance/Form1.vb
+++ Minerva Online Insurance/Minerva Online Insurance/Form1.vb
@@ -2,4 +2,12 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'comment
     End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Try
+            MsgBox("Address cannot be changed for this cloud application",vbInformation,"Notification")
+        Catch ex As Exception
+
+        End Try
+    End Sub
 End Class
